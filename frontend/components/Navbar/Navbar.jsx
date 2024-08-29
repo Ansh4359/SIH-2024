9779e11aeb475c32d87b52449d42c1c870a9268e
Ansh singh <anshsingh4359@gmail.com>
2024-08-29 12:12:28 +0530

    first commit

---
+++ frontend/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='flex flex-col'>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+    </nav>
+  )
+}
+
+export default Navbar
